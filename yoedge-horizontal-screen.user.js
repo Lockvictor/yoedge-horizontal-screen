bd1703fe83788acd597b70a2837b1b6257f9bade
Lockvictor <lucao.victor7@gmail.com>
2017-04-06 01:05:27 +0800

    仅用一个遮罩覆盖canvas，实现滚轮缩放和点击翻页

--- yoedge-horizontal-screen.user.js
+++ yoedge-horizontal-screen.user.js
@@ -11,77 +11,125 @@
 // @grant       none
 // ==/UserScript==
 
-var SCALE_RATIO = 0.5; //画布宽度占屏幕宽度的比例
-var WIDTH = screen.width * SCALE_RATIO;
-var HEIGHT = WIDTH * 1.5;
+var MANGA_ASPECT_RATIO = 1.5; //漫画宽高比
+
+var gMangaAreaRatio = 0.5; //漫画宽度占屏幕宽度的比例
+var MAX_SCALE_RATIO = 1; //最大缩放比例，与屏幕等宽
+var MIN_SCALE_RATIO = 0.5; //最小缩放比例，屏幕的30%
+var SCALE_STEP = 0.05;
+
+var PAGE_BUTTON_AREA_RATIO = 0.1; //顶部和底部响应翻页事件的区域比例，10%
 
 
 (function () {
     'use strict';
-    // 根据工具栏的有无判断漫画是否已经加载
+    // 加载后调整工具栏位置
     var settingButtonFlag;
     settingButtonFlag = setInterval(function () {
         var settingButton = document.getElementById('normal-button').parentElement;
         if (settingButton !== null) {
             settingButton.style.position = 'fixed';
             clearInterval(settingButtonFlag);
-            main();
         }
     }, 1000);
-})();
 
+    // main
+    var canvasObj = document.getElementsByTagName('canvas')[0];
+    var containerObj = canvasObj.parentElement;
 
-function main() {
-    var canvasObj = document.getElementsByTagName('canvas')[0],
-        containerObj = canvasObj.parentElement;
-
-    // 调整canvas和container
-    resizeCanvas(canvasObj, containerObj);
-
-    // 构造遮罩屏蔽canvas缩放
-    addMask(containerObj);
-}
-
-
-function resizeCanvas(canvasObj, containerObj) {
-    canvasObj.style.width = WIDTH + 'px';
-    canvasObj.style.height = HEIGHT + 'px';
+    // 调整container
     containerObj.style.width = '100%';
     containerObj.style.height = 'auto';
     containerObj.style.margin = '0';
     containerObj.style.textAlign = 'center';
+
+    // 调整canvas
+    scaleCanvas(canvasObj, 0);
+
+    // 添加遮罩覆盖canvas的缩放和翻页
+    addCanvasMask(canvasObj, containerObj);
+})();
+
+
+function scaleCanvas(canvasObj, increment) {
+    if (increment > 0) {
+        gMangaAreaRatio = (gMangaAreaRatio >= MAX_SCALE_RATIO) ? MAX_SCALE_RATIO : (gMangaAreaRatio + increment);
+    } else if (increment < 0) {
+        gMangaAreaRatio = (gMangaAreaRatio <= MIN_SCALE_RATIO) ? MIN_SCALE_RATIO : (gMangaAreaRatio + increment);
+    }
+    console.log('gMangaAreaRatio = ' + gMangaAreaRatio);
+    var newWidth = screen.width * gMangaAreaRatio;
+    var newHeight = newWidth * MANGA_ASPECT_RATIO;
+
+    canvasObj.style.width = newWidth + 'px';
+    canvasObj.style.height = newHeight + 'px';
+
+    var canvasMask = document.getElementById('canvasMask');
+    if (canvasMask !== null) {
+        canvasMask.style.width = newWidth + 'px';
+        canvasMask.style.height = newHeight + 'px';
+        canvasMask.style.marginLeft = - newWidth / 2 + 'px';
+    }
 }
 
 
-function addMask(containerObj) {
-    var pagerButtonWidth = 80;
-    var pagerButtonHeight = 50;
+function addCanvasMask(canvasObj, containerObj) {
+    var canvasWidth = screen.width * gMangaAreaRatio;
+    var canvasHeight = canvasWidth * MANGA_ASPECT_RATIO;
 
-    var maskList = [];
-    // 通用样式
-    for (let i = 0; i < 3; i++) {
-        maskList[i] = document.createElement('div');
-        let mask = maskList[i];
-        mask.width = (WIDTH - pagerButtonWidth) / 2
-        mask.style.width = mask.width + 'px';
-        mask.height = HEIGHT;
-        mask.style.height = mask.height + 'px';
-        mask.style.zIndex = 1;
-        mask.style.position = 'absolute';
-        mask.style.top = '0';
-        mask.style.left = '50%';
-        // mask.style.backgroundColor = 'rgba(211, 211, 211, 0.3)';
-    }
-    // 左侧遮罩
-    maskList[0].style.marginLeft = -WIDTH / 2 + 'px';
-    // 中间遮罩
-    maskList[1].style.marginLeft = - maskList[1].width / 2 + 'px';
-    maskList[1].style.height = (HEIGHT - 2 * pagerButtonHeight) + 'px';
-    maskList[1].style.top = pagerButtonHeight + 'px';
-    // 右侧遮罩
-    maskList[2].style.marginLeft = pagerButtonWidth / 2 + 'px';
+    var canvasMask = document.createElement('div');
+    canvasMask.id = 'canvasMask';
+    canvasMask.style.width = canvasWidth + 'px';
+    canvasMask.style.height = canvasHeight + 'px';
+    canvasMask.style.zIndex = 1;
+    canvasMask.style.position = 'absolute';
+    canvasMask.style.top = '0';
+    canvasMask.style.left = '50%';
+    canvasMask.style.marginLeft = - canvasWidth / 2 + 'px';
+    canvasMask.style.backgroundColor = 'rgba(211, 211, 211, 0.3)';
 
-    for (let i = 0; i < 3; i++) {
-        containerObj.appendChild(maskList[i]);
-    }
+    //添加鼠标滚轮缩放事件
+    addMousewheelEventListener(canvasObj, canvasMask);
+
+    //添加鼠标点击翻页事件
+    addPageTurningEventListener(canvasObj, canvasMask)
+
+    containerObj.appendChild(canvasMask);
+}
+
+function addMousewheelEventListener(canvasObj, canvasMask) {
+    //一般浏览器的鼠标滚轮事件
+    canvasMask.addEventListener('mousewheel', function (event) {
+        // console.log(event);
+        if (event.deltaY < 0) {//放大
+            scaleCanvas(canvasObj, SCALE_STEP);
+        } else if (event.deltaY > 0) { //缩小
+            scaleCanvas(canvasObj, -SCALE_STEP);
+        }
+        event.stopPropagation();
+        event.preventDefault();
+    });
+
+    // 火狐的鼠标滚轮事件
+    canvasMask.addEventListener('DOMMouseScroll', function (event) {
+        // console.log(event);
+        if (event.detail < 0) {//放大
+            scaleCanvas(canvasObj, SCALE_STEP);
+        } else if (event.detail > 0) { //缩小
+            scaleCanvas(canvasObj, -SCALE_STEP);
+        }
+        event.stopPropagation();
+        event.preventDefault();
+    });
+}
+
+function addPageTurningEventListener(canvasObj, canvasMask) {
+    canvasMask.addEventListener('mouseup', function (event) {
+        // console.log(event);
+        var canvasHeight = screen.width * gMangaAreaRatio * MANGA_ASPECT_RATIO;
+        var pageButtonAreaHeight = canvasHeight * PAGE_BUTTON_AREA_RATIO;
+        if (event.layerY <= pageButtonAreaHeight || event.layerY >= (canvasHeight - pageButtonAreaHeight)) {
+            canvasObj.dispatchEvent(new MouseEvent(event.type, event));
+        }
+    });
 }
